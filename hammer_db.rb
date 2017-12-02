79720a044fd9958adeaf3cce482a58414528d491
Hormoz K <hkheradm@gmail.com>
2017-12-02 14:08:26 -0500

    update load tester

--- hammer_db.rb
+++ hammer_db.rb
@@ -2,6 +2,9 @@ require 'mysql2'
 
 conn = Mysql2::Client.new(host: "127.0.0.1", username: "root", port: 21001)
 
+conn.query("CREATE DATABASE IF NOT EXISTS test")
+conn.query("CREATE TABLE IF NOT EXISTS test.test (id INT, PRIMARY KEY(id))")
+
 loop do
   conn.query("INSERT IGNORE INTO test.test VALUES(#{Integer(rand * 99999999999)})")
 end
